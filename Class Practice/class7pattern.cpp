21eb55597d2d6dc30c641ecbf04c1d216a952fd2
sahil <sahilmendiratta222@gmail.com>
2022-10-25 01:07:51 +0530

    Pattern question

---
+++ Class Practice/class7pattern.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int m,n;
+    cin>>n>>m;
+    // Rectangular pattern
+    // for(int i=1;i<=n;i++)
+    // {
+    //     for(int j=1;j<=m;j++)
+    //     {
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+    // }
+    // 2nd
+    // *****
+    // *   *
+    // *   *
+    // *****
+    // for(int i=1;i<=n;i++)
+    // {
+    //     for(int j=1;j<=m;j++)
+    //     {
+    //         if(i==1 || i==n || j==1 || j==m)
+    //         {
+    //             cout<<"*";
+    //         }
+    //         else
+    //             cout<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+    // 3rd pattern Triangular pattern
+    // 4th 
+    // for(int i=1;i<=n;i++)
+    // {
+    //     for(int j=i;j<=n;j++)
+    //     {
+    //         cout<<j;
+    //     }
+    //     if(i>1)
+    //     {
+    //         for(int k=1;k<i;k++)
+    //         {
+    //             cout<<k;
+    //         }
+    //     }
+    //     cout<<endl;
+    // }
+    // 5th
+    for(int i=0;i<=m;i++)
+    {
+        for(int j;j<=m;j++)
+        {
+            if(i==1|| j==m || j==1 || i==n)
+            {
+                
+            }
+        }
+    }
+
+}

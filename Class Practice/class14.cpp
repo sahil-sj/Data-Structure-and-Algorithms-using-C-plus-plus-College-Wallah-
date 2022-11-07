adc0022e430f1ad9cb1ec8726c02ec785df0daa8
sahil <sahilmendiratta222@gmail.com>
2022-11-08 02:43:04 +0530

    Problem Based Lecture

---
+++ Class Practice/class14.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // 1 Target sum
+    int arr[5];
+    for(int i=0;i<5;i++)
+    {
+        cin>>arr[i];
+    }
+    int tsum;
+    cout<<"Enter target sum : "<<endl;
+    // cin>>tsum;
+    // int count=0;
+    // for(int i=0;i<5;i++)
+    // {
+    //     for(int j=i+1;j<5;j++)
+    //     {
+    //         if (arr[i]+arr[j]==tsum)
+    //         {
+    //             count++;
+    //         }
+    //     }
+    // }
+    // cout<<"The Pairs are : "<<count<<endl;
+
+    // 2 Target sum of Triplets
+
+
+
+}
